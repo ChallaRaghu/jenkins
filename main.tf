7660a07d9c973412f0735c80a7ae743e307ce3c5
Ch. Raghu Chandra Reddy <123090081+ChallaRaghu@users.noreply.github.com>
2024-12-04 12:58:58 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "dev" {
+    ami = "ami-0614680123427b75e"
+    instance_type = "t2.micro"
+    key_name="mykey"
+    tags = {
+      Name = "dev-ec3"
+    }
+}
